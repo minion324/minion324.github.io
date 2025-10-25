12465bab29dbe6d33e83fdcf41c8f3d9eeb62434
minion324 <ktn5179@psu.edu>
2025-10-25 14:35:54 -0400

    starter file

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+  return "<h1>hello world</h1>"
+
+
+
+@app.route('/hello')
+
+def hello():
+  return "Hello World"
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5555, debug=True)
+
+
