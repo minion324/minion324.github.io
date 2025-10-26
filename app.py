a97ca11eb9732b68dc0031df3391408f93acb2bb
minion324 <ktn5179@psu.edu>
2025-10-26 03:55:05 -0400

    Made everything look pretty and made a proper base html.

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder="templates", static_folder='static', static_url_path='/')
 @app.route('/')
-
 def index():
-  return "<h1>hello world</h1>"
+    return render_template("timer.html")
 
-
-
-@app.route('/hello')
-
-def hello():
-  return "Hello World"
 
 
 if __name__ == '__main__':
